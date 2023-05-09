e2f8e2b554cbe190cc5df7c9ad439cb134acc738
Jason Milad <jasonmilad29@gmail.com>
2023-05-08 23:21:21 -0400

    initial, memoized but unoptimized solution

---
+++ dp/198_house_robber.py
@@ -0,0 +1,15 @@
+class Solution:
+    def robHelper(self, i, nums, memo) -> int:
+        if i==len(nums)-1:
+            return nums[i]
+        elif i>len(nums)-1:
+            return 0
+        elif memo[i] != None:
+            return memo[i]
+        else:
+            memo[i] = max(nums[i] + self.robHelper(i+2, nums, memo), nums[i+1] + self.robHelper(i+3, nums, memo))
+            return memo[i]
+            
+    def rob(self, nums: List[int]) -> int:
+        memo = [None] * len(nums)
+        return self.robHelper(0, nums, memo)
